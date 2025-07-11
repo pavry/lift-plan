9de0c0911374332dadb3277b60037e6743d63584
Pavlo Rymarchuk <43524127+pavry@users.noreply.github.com>
2025-07-11 12:25:14 +0300

    add direct base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/lift-plan/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
